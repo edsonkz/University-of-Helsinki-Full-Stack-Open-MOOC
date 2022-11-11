96b95432eb28a3f31873e27de7ed31ad7a8d59dd
Edson Cavalcante de Albuquerque Bisneto <edsonbisneto12@gmail.com>
2022-11-11 12:41:10 -0300

    Cleared exercises 3.1 to 3.8.

---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+let persons = [
+    { 
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
+      },
+      { 
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+      },
+      { 
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+      },
+      { 
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+      }
+];
+
+const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+
+
+app.use(express.json());
+
+morgan.token('body', req => {
+    return JSON.stringify(req.body)
+  })
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+app.get("/api/persons", (request, response)=> {
+    response.send(persons);
+});
+
+app.get("/info", (request, response)=> {
+    response.send(`<div>
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+    </div>`);
+});
+
+app.get("/api/persons/:id", (request, response)=> {
+    const id = request.params.id;
+    let person = persons.find(person => person.id === Number(id)); 
+
+    if(!person){
+        response.status(404).send({error: "Person not found with that id."});
+    }
+
+    response.send(person);
+});
+
+app.post("/api/persons", (request, response) => {
+    const body = request.body;
+
+    if(!body.name || !body.number)
+        response.status(400).send({error: "Name and number are required. Check if you request define those attributes."});
+    else if(persons.find(person => person.name === body.name))
+        response.status(400).send({error: "Name must be unique."});
+
+    let {name, number} = body;
+    let newPerson = {
+        id: generateId(),
+        name,
+        number,
+    }
+
+    persons = persons.concat(newPerson);
+
+    response.status(201).send(newPerson);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+    const id = request.params.id;
+    persons = persons.filter(person => person.id !== Number(id)); 
+
+    response.send("Person was succefully deleted");
+});
+
+
+app.listen(3001);
